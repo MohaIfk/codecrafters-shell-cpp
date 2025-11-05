5c5258db675b782d9fe66684b38d8515e3884842
Mohammed Ifkirne <161834771+MohaIfk@users.noreply.github.com>
2025-11-05 12:51:45 +0100

    Handle invalid commands

--- src/main.cpp
+++ src/main.cpp
@@ -8,4 +8,7 @@ int main() {
 
   // TODO: Uncomment the code below to pass the first stage
   std::cout << "$ ";
+  std::string cmd;
+  std::getline(std::cin, cmd);
+  std::cout << cmd << ": command not found" << std::endl;
 }
